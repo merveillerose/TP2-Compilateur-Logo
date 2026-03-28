2a96ed876eb6a77a50ac1eb4ac0b65436d87a6ae
Merveille Yomba <mira605175@gmail.com>
2026-03-28 07:18:01 +0100

    Partie 1: Grammaire Logo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    // programme qui respecte la grammaire pour construire un carré
+    let logo_program = "
+        forward 100
+        right 90
+        forward 100
+        right 90
+        forward 100
+        right 90
+        forward 100
+        right 90
+    ";
+
+    println!("Programme Logo :\n{}", logo_program);
+
 }
